ec34f37063a1a16bfec4363be617ea109e9b902f
Michael Zhou <michael.zhou@gmail.com>
2012-10-25 08:17:31 -0700

    Changes for HW3

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -22,14 +22,15 @@ Background: movies have been added to database
   And  I am on the RottenPotatoes home page
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+
+  Given I check the following ratings: PG, R
+  And I uncheck the following ratings: G, PG-13
+  When I press "ratings_submit"
+  Then I should see all movies of the ratings: PG, R
+  And I should not see any movies of the ratings: PG-13, G
 
 Scenario: no ratings selected
-  # see assignment
+  Then I should see all of the movies
 
 Scenario: all ratings selected
-  # see assignment
+  Then I should see all of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,16 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
+  Movie.delete_all
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    m = Movie.new
+    m.title = movie['title']
+    m.rating = movie['rating']
+    m.release_date = movie['release_date']
+    m.save!
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,15 +19,57 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  ie1 = page.body.index e1
+  ie2 = page.body.index e2
+  if ie1.nil? or ie2.nil? or ie1 > ie2
+    raise 'Wrong order of movie listing'
+  end
 end
 
+# Make sure we can see all the movies
+
+Then /I should see all of the movies/ do
+  # Count all table rows
+  if page.body.scan("<tr>").length != Movie.count()+1
+    raise 'Wrong number of movies'
+  end
+end
+
+# Make sure we can see all the movies of the specified ratings
+
+Then /I should see all movies of the ratings: (.*)$/ do |rating_list|
+  rating_list.split(',').each do |rating|
+    Movie.find_all_by_rating(rating.strip).each do |movie|
+      if not page.body =~ /#{movie.title}/
+        raise "Expecting but found no movie title: #{movie.title}"
+      end
+    end
+  end
+end
+
+# Make sure we cannot see any movies of the specified ratings
+
+Then /I should not see any movies of the ratings: (.*)$/ do |rating_list|
+  rating_list.split(',').each do |rating|
+    Movie.find_all_by_rating(rating.strip).each do |movie|
+      if page.body =~ /#{movie.title}/
+        raise "Not expecting but found movie title: #{movie.title}"
+      end
+    end
+  end
+end
+
+#
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+Given /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(',').each do |rating|
+    step %{I #{uncheck}check "ratings_#{rating.strip}"}
+  end
 end
+
